75041a6f0e066ff7106179b73a6387ee6056e1f4
AnujDagade <dagadeanuj@gmail.com>
2022-10-22 09:48:50 +0000

    APKv2

---
+++ app/build/generated/source/buildConfig/androidTest/debug/com/example/accountapp/test/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.accountapp.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "mooc.vandy.java4android.Account.test";
+  public static final String BUILD_TYPE = "debug";
+}
